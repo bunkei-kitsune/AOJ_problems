e69d51850f4d48f05221cb58ea4a5d991f3de40c
bunkei-kitsune <ai.kks0214@gmail.com>
2021-07-14 22:39:06 +0900

    ITP1_3_Aの答え

---
+++ ITP1_3_A.py
@@ -0,0 +1,11 @@
+# 1000 個の "Hello World" を出力するプログラムを作成して下さい。
+
+# for文で繰り返すパターン
+for i in range(1000):
+    print('Hello World')
+
+# while文で繰り返すパターン
+i = 0
+while i < 1000:
+    print('Hello World')
+    i += 1
